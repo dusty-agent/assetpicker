801d25217421e9d904367c4f029d48cd52cafbb0
dustydraft <soyoung@dustydraft.com>
2026-07-14 07:04:07 +0900

    feat: automate AP Daily email delivery with GitHub Actions

---
+++ restore_daily4.ps1
@@ -0,0 +1,5 @@
+$history="$env:APPDATA\Code\User\History"
+
+Copy-Item "$history\4f1e0d78\GFfu.html" "app\daily\templates\issue_list.html"
+
+Copy-Item "$history\2bcea170\08cQ.html" "app\daily\templates\summary.html"
